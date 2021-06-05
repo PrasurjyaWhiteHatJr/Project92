6e5f5a0b21a5e4df3c0c628f00f6941d6b656d9a
prasurjya <65493249+PrasurjyaWhiteHatJr@users.noreply.github.com>
2021-06-05 13:06:38 +0530

    Add files via upload

---
+++ RandomScreen.js
@@ -0,0 +1,10 @@
+import React from react-native
+import {StyleSheet, Text, Image, View} from react-native
+
+export default class extends RandomScreen from React.Component(
+    render(){
+        return(
+            <View>
+        )
+    }
+)
